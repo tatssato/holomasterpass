d6209ed384bbe992cc476c5d26ecf9caaf5f701d
Jonatan Ramhöj <jonatan.ramhoj@gmail.com>
2020-01-12 12:19:25 +0000

    Add matUI

--- gui/src/pages/Passwords.js
+++ gui/src/pages/Passwords.js
@@ -1,74 +1,28 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import AppShell from '../components/AppShell';
-// import AddIcon from '../icons/Add';
 import HoloBridge from '../client-api/api';
 import { withRouter } from 'react-router';
 
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: 800px;
-  margin: 0 auto;
-`;
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Typography,
+  FormGroup,
+  TextField,
+  FormControl,
+  Button
+} from '@material-ui/core';
 
-const List = styled.ul`
-  list-style: none;
-`;
-
-const Url = styled.span`
-  font-size: 32px;
-`;
-
-const Password = styled.span`
-  font-size: 32px;
-  display: none;
-`;
-
-const ListItem = styled.li`
-  margin-bottom: 16px;
-  cursor: pointer;
-  &:hover ${Url} {
-    display: none;
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    marginBottom: theme.spacing(2)
+  },
+  button: {
+    fontWeight: 'bold',
+    padding: theme.spacing(2)
   }
-  &:hover ${Password} {
-    display: block;
-  }
-`;
-
-const Button = styled.button`
-  background: #813cff;
-  color: #fff;
-  width: 100%;
-  padding: 20px;
-  border-radius: 3px;
-  text-align: center;
-  cursor: pointer;
-  display: inline-block;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const Grid = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const GridItem = styled.div`
-  margin: 20px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 20px;
-  border: 1px solid #333;
-  border-radius: 3px;
-  box-sizing: border-box;
-  margin-bottom: 20px;
-  font-size: 18px;
-`;
+}));
 
 function Passwords({ history }) {
   const [passDetails, setPassDetails] = useState([])
@@ -82,10 +36,9 @@ function Passwords({ history }) {
   }
 
   const onSubmit = async e => {
-    const passNameVal = passNameInput.current.value
-    const typeVal = typeInput.current.value || 'medium'
-    const counterVal = counterInput.current.value || +1
-    debugger
+    const passNameVal = document.getElementById('passNameInput').value;
+    const typeVal = document.getElementById('typeInput').value || 'medium';
+    const counterVal = document.getElementById('counterInput').value || +1;
     if (passNameVal.length) {
       const result = await HoloBridge.savePassDetailEntry(passNameVal, typeVal, +counterVal)
       setPassDetails([...passDetails, result.newPassEntry])
@@ -96,29 +49,57 @@ function Passwords({ history }) {
   // TODO discuss when the passwds are generated (one at a time on reveal or all at once on render)
   // TODO make counter an increment widget
   // TODO make pw_typ a pulldown
-  debugger
   return (
     <AppShell>
-      <Wrapper>
-        <Grid>
-          <GridItem>
-            <Input placeholder="Name" ref={passNameInput} />
-            <Input placeholder="Type" ref={typeInput} />
-            <Input placeholder="Counter" ref={counterInput} />
-            <Button onClick={onSubmit}>Add new password</Button>
-
-            <List>
-              {passDetails.length ? passDetails.map((item) => (
-                <ListItem key={`${item.name}-${item.counter}`}>
-                  <Url className='url'>{item.name}</Url>
-                  <Password className='password'>{HoloBridge.generatePassFromPD(item)}</Password>
-                </ListItem>
-              )): null }
-            </List>
-
-          </GridItem>
-        </Grid>
-      </Wrapper>
+      <Box mx="auto" maxWidth="md">
+        <Box component="form">
+          <FormControl fullWidth className={classes.formControl}>
+            <TextField
+              label="Site name"
+              placeholder="Site name (eg. github.com)"
+              required
+              variant="outlined"
+              fullWidth
+              id="passNameInput"
+            />
+          </FormControl>
+          <FormControl fullWidth className={classes.formControl}>
+            <TextField
+              label="Type"
+              placeholder="Type"
+              required
+              variant="outlined"
+              fullWidth
+              id="typeInput"
+            />
+          </FormControl>
+          <FormControl fullWidth className={classes.formControl}>
+            <TextField
+              label="Type"
+              placeholder="Counter"
+              required
+              variant="outlined"
+              fullWidth
+              id="counterInput"
+            />
+          </FormControl>
+          <Button
+            onClick={onSubmit}
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.button}
+            >Add new password</Button>
+          <Box>
+            {passDetails.length ? passDetails.map((item, index) => (
+              <Box key={`${index}-${item.counter}`}>
+                <Box className='Box'>{item.name}</Box>
+                <Box className='Box'>{HoloBridge.generatePassFromPD(item)}</Box>
+              </Box>
+            )): null }
+          </Box>
+        </Box>
+      </Box>
     </AppShell>
   );
 }
